a30f1540a6ea56a550abc03b2e48f0bade77b519
tomerdayan168 <tomerdayan168@gmail.com>
2021-12-19 11:54:46 +0200

    add socket listener

---
+++ pages/api/socketio.js
@@ -0,0 +1,27 @@
+import { Server } from "socket.io";
+
+const ioHandler = (req, res) => {
+  if (!res.socket.server.io) {
+    console.log("First use, starting socket.io");
+
+    const io = new Server(res.socket.server);
+
+    io.on("connection", (socket) => {
+      socket.on("log", (msg) => {
+        // Do something with log msg
+      });
+    });
+
+    res.socket.server.io = io;
+  } 
+
+  res.end();
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default ioHandler;
