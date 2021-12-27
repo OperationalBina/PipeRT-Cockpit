58e09fa08af589df760c83801f55e0fb39777f9a
Elon-Abulafia <elon945@gmail.com>
2021-12-27 12:41:03 +0200

    Change 'data' to 'message'

--- components/logs-display-view.js
+++ components/logs-display-view.js
@@ -2,12 +2,12 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Dialog from "@mui/material/Dialog";
 import DialogContentText from '@mui/material/DialogContentText';
 
-export default function LogDisplayView({ is_open, level, data }) {
+export default function LogDisplayView({ is_open, level, message }) {
   return (
     <Dialog open={is_open}>
       <DialogTitle>{level}</DialogTitle>
       <DialogContentText>
-        {data}
+        {message}
       </DialogContentText>
     </Dialog>
   );
