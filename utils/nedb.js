9019dc17ce947b9778580b8dd00e027b9443291f
Elon-Abulafia <elon945@gmail.com>
2021-12-27 12:40:20 +0200

    Add persistent db support

--- utils/nedb.js
+++ utils/nedb.js
@@ -7,81 +7,18 @@ function connectToDatabase() {
     return { db: cachedDb }
   }
 
-  var db = {};
+  const db = {};
 
-  db.plogs = new Datastore();
-  db.debugs = new Datastore();
-  db.infos = new Datastore();
-  db.warnings = new Datastore();
-  db.exceptions = new Datastore();
-  db.routines = new Datastore();
-
-  setInitialData(db)
+  db.debugs = new Datastore({ filename: 'debugs.db', autoload: true });
+  db.infos = new Datastore({ filename: 'infos.db', autoload: true });
+  db.warnings = new Datastore({ filename: 'warnings.db', autoload: true });
+  db.exceptions = new Datastore({ filename: 'exceptions.db', autoload: true });
+  db.routines = new Datastore({ filename: 'routines.db', autoload: true });
+  db.pipe_infrastructures = new Datastore({ filename: 'pipe_infrastructures.db', autoload: true });
 
   cachedDb = db
 
   return { db }
-}
-
-function setInitialData(db) {
-  insert(db.routines, [
-    { error_level: 0, name: `InitialRoutine_1` },
-    { error_level: 1, name: `InitialRoutine_2` },
-    { error_level: 2, name: `InitialRoutine_3` }
-  ]);
-
-  const logs = [
-    {
-      Source: 'InitialRoutine_1',
-      level: "Exception",
-      data: "I really love using pipeRT, I really love using pipeRT, I really love using pipeRT, I really love using pipeRT, I really love using pipeRT",
-    },
-    {
-      Source: 'InitialRoutine_1',
-      level: "Exception",
-      data: "I really love using pipeRT",
-    },
-    {
-      Source: 'InitialRoutine_1',
-      level: "Exception",
-      data: "I really love using pipeRT",
-    },
-    {
-      Source: 'InitialRoutine_1',
-      level: "Warning",
-      data: "I really love using pipeRT",
-    },
-    {
-      Source: 'InitialRoutine_1',
-      level: "Info",
-      data: "I really love using pipeRT",
-    },
-    {
-      Source: 'InitialRoutine_1',
-      level: "Exception",
-      data: 'Handled at stack lvl 0 \n \
-      File "exc.py", line 17, in <module> \n \
-        stack_lvl_1() \n \
-      File "exc.py", line 13, in stack_lvl_1 \n \
-        stack_lvl_2() \n \
-      File "exc.py", line 9, in stack_lvl_2 \n \
-        stack_lvl_3() \n \
-      File "exc.py", line 5, in stack_lvl_3 \n\
-        raise Exception("a1", "b2", "c3")',
-    },
-  ]
-
-  const logs_by_level = logs.reduce(function (r, a) {
-    let log_level = a.level.toLowerCase() + "s"
-    r[log_level] = r[log_level] || [];
-    r[log_level].push(a);
-    return r;
-  }, Object.create(null));
-
-  for (const [log_level, logs] of Object.entries(logs_by_level)) {
-    insert(db[log_level], logs)
-  }
-
 }
 
 function insert(db, ...opt) {
@@ -119,6 +56,5 @@ function count(db, ...opt) {
     })
   })
 }
-
 
 export { connectToDatabase, insert, find, count }
