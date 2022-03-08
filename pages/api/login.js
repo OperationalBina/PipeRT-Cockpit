7d85c9e97eee929f741ee4252cc8dd17a5c4c935
tomerdayan168 <tomerdayan168@gmail.com>
2022-03-08 11:35:42 +0200

    Change to bcrypt library

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,14 @@
 import * as jwt from "jsonwebtoken";
 import { serialize } from "cookie";
 import { USERS, JWT_EXPIRATION, JWT_KEY } from "../../config";
-import * as CryptoJS from "crypto-js";
+import * as bcrypt from "bcrypt";
 
 function validate(username, password) {
   const usersJson = JSON.parse(USERS);
 
   if (USERS.length == 0) {
     throw "Users dosn't exist";
-  } else if (usersJson[username] == password) {
+  } else if (bcrypt.compareSync(password, usersJson[username])) {
     return true;
   } else {
     return false;
@@ -20,9 +20,7 @@ export default async function handler(req, res) {
   const username = reqBody["username"];
   const password = reqBody["password"];
 
-  const hashedPassword = CryptoJS.SHA256(password).toString()
-
-  if (validate(username, hashedPassword)) {
+  if (validate(username, password)) {
     const token = jwt.sign({ username }, JWT_KEY, {
       algorithm: "HS256",
       expiresIn: JWT_EXPIRATION,
