b02757b8c6f9b2b558a08c08c158ccee00165710
tomerdayan168 <33786803+tomerdayan168@users.noreply.github.com>
2022-03-10 11:18:19 +0200

    remove console log

--- utils/http-calls.js
+++ utils/http-calls.js
@@ -1,26 +1,22 @@
-import { SERVER_URL } from '../config';
+import { SERVER_URL } from "../config";
 
 export async function apiFetch(endpoint) {
-    let response = await fetch(`${SERVER_URL}/api/${endpoint}`);
+  let response = await fetch(`${SERVER_URL}/api/${endpoint}`);
 
-    if (response.ok) {
-        return response.json()
-        
-    } else 
-        throw `An error when fetching from /api/${endpoint} - ${response.text}`
+  if (response.ok) {
+    return response.json();
+  } else
+    throw `An error when fetching from /api/${endpoint} - ${response.text}`;
 }
 
 export async function apiPost(endpoint, params) {
-    let response = await fetch(`${SERVER_URL}/api/${endpoint}`, {
-        method: 'POST',
-        body: JSON.stringify({
-            ...params})
-    });
+  let response = await fetch(`${SERVER_URL}/api/${endpoint}`, {
+    method: "POST",
+    body: JSON.stringify({
+      ...params,
+    }),
+  });
 
-    console.log(response)
-
-    if (response.ok) 
-        return response.json()
-    else 
-        throw `An error when fetching from /api/${endpoint} - ${response.text}`
+  if (response.ok) return response.json();
+  else throw `An error when fetching from /api/${endpoint} - ${response.text}`;
 }
